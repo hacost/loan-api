5d05cb1e9e14eb7d4c4698ff81df3817be1f9037
Héctor Acosta <hacost@hotmail.com>
2022-01-28 23:37:17 -0600

    create status model

---
+++ db/models/status-model.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const STATUS_TABLE = 'status';
+
+// structure in database
+const StatusSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true
+  },
+  active: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  createAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Date.now()
+  },
+}
+
+// status model
+class Status extends Model {
+  static associate(){
+  }
+  static config(sequelize){
+    return {
+      sequelize,
+      tableName: STATUS_TABLE,
+      modelName: 'Status',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { Status, StatusSchema, STATUS_TABLE };
