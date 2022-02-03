86eeb7c54405ebd2ed58719a20f1d70b59ba4e07
Héctor Acosta <hacost@hotmail.com>
2022-02-02 23:56:18 -0600

    add all model association with loan

--- db/models/loan-model.js
+++ db/models/loan-model.js
@@ -1,4 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
+const { COORDINATOR_TABLE } = require('./coordinator-model');
+const { WALLET_TABLE } = require('./wallet-model');
+const { CUSTOMER_TABLE } = require('./customer-model');
+const { MONEY_COLLECTOR_TABLE } = require('./money-collector-model');
+const { STATUS_TABLE } = require('./status-model');
 const LOAN_TABLE = 'loans';
 
 //structure in database
@@ -81,28 +86,93 @@ const LoanSchema = {
   },
   // foreign keys 
   walletId:{
-
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'wallet_id',
+    references: {
+      model: WALLET_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   },
   customerId: {
-
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'customer_id',
+    references: {
+      model: CUSTOMER_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   },
   moneyCollectorId: {
-
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'money_collector_id',
+    references: {
+      model: MONEY_COLLECTOR_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  },
+  // add this field to coordinator can see and aprove or not to Loan
+  coordinatorId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'coordinator_id',
+    references: {
+      model: COORDINATOR_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   },
   approvedBy: {
-
+    type: DataTypes.INTEGER,
+    field: 'approved_by',
+    references: {
+      model: COORDINATOR_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   },
   canceledBy: {
-
+    type: DataTypes.INTEGER,
+    field: 'canceled_by',
+    references: {
+      model: COORDINATOR_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   },
   statusId:{
-
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'status_id',
+    references: {
+      model: STATUS_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL' 
   }
 }
 
 //loan model
 class Loan extends Model {
-  static associate(){
+  static associate(models){
+    this.belongsTo(models.Wallet, {as: 'wallet'});
+    this.belongsTo(models.Customer, {as: 'customer'});
+    this.belongsTo(models.MoneyCollector, {as: 'money-collector'});
+    this.belongsTo(models.Coordinator, {as: 'coordinator'});
+    this.belongsTo(models.Coordinator, {as: 'approved-by'});
+    this.belongsTo(models.Coordinator, {as: 'canceled-by'});
+    this.belongsTo(models.Status, {as: 'status'});
   }
 
   static config(sequelize){
